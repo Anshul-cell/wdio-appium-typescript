026ada1ceb8af61edfd7495a73ff9db86734c63b
Anshul Rawat <139808095+Anshul-cell@users.noreply.github.com>
2025-05-08 18:53:41 +0530

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -28,8 +28,7 @@ export const config: WebdriverIO.Config = {
     // should work too though). These services define specific user and key (or access key)
     // values you need to put in here in order to connect to these services.
     //
-    user: process.env.gagandeepb,
-    key: process.env.KZCghqXcnDAkO3kuErqkRKoQGgzXzzV95L4gJSf7dIaJPpCzpL,
+   
     //
     // If you run your tests on Sauce Labs you can specify the region you want to run your tests
     // in via the `region` property. Available short handles for regions are `us` (default) and `eu`.
